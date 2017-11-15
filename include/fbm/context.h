6d70a804c77419147e9b6a012d5eadff976ab3c2
Sol Kim <defreturnnone@gmail.com>
2017-11-15 15:53:07 +0900

    fix for windows

--- include/fbm/context.h
+++ include/fbm/context.h
@@ -11,7 +11,7 @@ namespace FBM
     {
         public:
             Context(unsigned int octave)
-                : m_absolute(false), m_octave(0), m_amplitude(0), m_frequency(0), m_offset(0), m_noise(0) {};
+                : m_absolute(false), m_octave(octave), m_amplitude(0), m_frequency(0), m_offset(0), m_noise(0) {};
             Context(unsigned int octave, const Noise *noise, float amplitude, float frequency, float offset, bool absolute)
                 : m_absolute(absolute), m_octave(octave), m_amplitude(amplitude), m_frequency(frequency), m_offset(offset), m_noise(noise) {};
             ~Context() {};
